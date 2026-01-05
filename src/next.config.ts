336ee8c90f6ee5a8bb1eb4f0ffcd21f0f0fb6a63
rahulmadvisory <rahulm.advisory@gmail.com>
2026-01-06 00:15:07 +0530

    Revert "Reapply "Merge branch 'main' of https://github.com/rahulmadvisory/rahulmadvisory.github.io""
    This reverts commit d2370b7c1326d6fd22089dc3ed59337680f857e9.

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
-
-const nextConfig = {
-  basePath: '/docs',
-  assetPrefix: 'https://rahulmadvisory.github.io/docs/',
-  trailingSlash: true,
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+module.exports = {
   output: 'export',
-  images: {
-    unoptimized: true  // GitHub Pages doesn't support Next.js image optimization
-  }
-}
-module.exports = nextConfig
+};
+export default nextConfig;
