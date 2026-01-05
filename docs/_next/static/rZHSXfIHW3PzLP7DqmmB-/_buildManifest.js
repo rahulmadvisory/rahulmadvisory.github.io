f8c650c62b164966203969df75b15f4285c54674
rahulmadvisory <rahulm.advisory@gmail.com>
2026-01-06 00:05:23 +0530

    Revert "Merge branch 'main' of https://github.com/rahulmadvisory/rahulmadvisory.github.io"
    This reverts commit 14d47bdd8ccef1f6ccc3f700a1ca5be9f2e04899, reversing
    changes made to c91eac5cdb430d93c00636683a99d370a0935085.

--- docs/_next/static/rZHSXfIHW3PzLP7DqmmB-/_buildManifest.js
+++ docs/_next/static/rZHSXfIHW3PzLP7DqmmB-/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/docs//_next/:path+",
-        "destination": "/docs/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
